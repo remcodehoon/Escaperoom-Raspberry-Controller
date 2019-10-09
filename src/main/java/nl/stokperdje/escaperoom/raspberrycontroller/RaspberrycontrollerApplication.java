a58ae5e13c9e0ae99ef6e64cc6d1d7883b166b0f
Remco de Hoon <remcodehoon@gmail.com>
2019-10-09 13:31:41 +0200

    Init

---
+++ src/main/java/nl/stokperdje/escaperoom/raspberrycontroller/RaspberrycontrollerApplication.java
@@ -0,0 +1,13 @@
+package nl.stokperdje.escaperoom.raspberrycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberrycontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberrycontrollerApplication.class, args);
+	}
+
+}
